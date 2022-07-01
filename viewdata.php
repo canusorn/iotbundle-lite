0c24a62c0c4716e4a05c96c923cbd192b68d2b4e
canusorn <anusorn1998@gmail.com>
2022-07-01 15:52:18 +0700

    chart js

--- viewdata.php
+++ viewdata.php
@@ -20,12 +20,53 @@
         <p id="time"></p>
     </div>
 
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         $(document).ready(function() {
 
+            var tempdata = [];
+            var timedata = [];
+
+            // chart js
+            var data = {
+                labels: timedata,
+                datasets: [{
+                    label: 'temp',
+                    data: tempdata,
+                }]
+            };
+
+            var config = {
+                type: 'line',
+                data: data,
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        title: {
+                            display: true,
+                            text: 'Chart.js Line Chart'
+                        }
+                    }
+                },
+            };
+
+            var myChart = new Chart(
+                document.getElementById('myChart'),
+                config
+            );
 
 
+
+
+            // ajax for short polling
             function getnewdata() {
                 $.get("ajax.php", function() {})
                     .done(function(data) {
@@ -35,9 +76,16 @@
                         $("#temp").html(json.temp);
                         $("#time").html(json.time);
 
+                        tempdata.push(json.temp);
+                        timedata.push(json.time);
+
+                        myChart.data.datasets[0].data = tempdata;
+                        myChart.data.datasets[0].label = timedata;
+                        myChart.update();
+
                     })
                     .fail(function() {
-                        alert("error");
+                        console.log("error");
                     });
             }
             getnewdata();
